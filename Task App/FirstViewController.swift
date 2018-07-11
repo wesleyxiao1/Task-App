082b0495573892b60afa5bfdc5620f16f557719b
Wesley Xiao <wex057@ucsd.edu>
2018-07-11 12:01:06 -0700

    FirstViewController edit 1: added functions to count number of tasks, display tasks in the table, and delete tasks form table

--- Task App/FirstViewController.swift
+++ Task App/FirstViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tblTasks : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tblTasks.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,24 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskMgr.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "Default Tasks")
+        
+        cell.textLabel?.text = taskMgr.tasks[indexPath.row].name
+        cell.detailTextLabel?.text = taskMgr.tasks[indexPath.row].desc
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if(editingStyle == UITableViewCellEditingStyle.delete){
+            taskMgr.tasks.remove(at: indexPath.row)
+            tblTasks.reloadData()
+        }
+    }
 }
 
