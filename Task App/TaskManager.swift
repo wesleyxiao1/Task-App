a341308ca1b1a907e3504bfd03f82f0709d9e182
Wesley Xiao <wex057@ucsd.edu>
2018-07-11 11:08:14 -0700

    TaskManager class created

---
+++ Task App/TaskManager.swift
@@ -0,0 +1,24 @@
+//
+//  TaskManager.swift
+//  Task App
+//
+//  Created by Wesley on 7/11/18.
+//  Copyright © 2018 Wesley. All rights reserved.
+//
+
+import UIKit
+
+var taskMgr: TaskManager = TaskManager()
+
+struct task {
+    var name = "Name"
+    var desc = "Description"
+}
+
+class TaskManager: NSObject {
+    var tasks = [task]()
+    
+    func addTask(name: String, desc: String) {
+        tasks.append(task(name: name, desc: desc))
+    }
+}
